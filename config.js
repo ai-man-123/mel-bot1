79fac24ceb9b6d37605da222174ddfbfbf6d46b6
AIMAN *͘⁴̅⁰͍⁴̵〆* <82744566+ai-man-123@users.noreply.github.com>
2021-11-16 14:09:14 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 let fs = require('fs')
 let chalk = require('chalk')
-global.owner = ['6287755080455', '62882003806038'] // Nomor Owner
-global.autoread = false // false untuk menonaktifkan autoread, true untuk menyalakan autoread
+global.owner = ['60102810046', '436507776335335'] // Nomor Owner
+global.autoread = true // false untuk menonaktifkan autoread, true untuk menyalakan autoread
 global.selfmode = false // false = Mode Publik, true = Mode Self
 global.thumb = './thumb/itsuki.jpg'
-global.fakereplyt = 'Mel-Bot WhatsApp' // Teks Untuk Fake Reply
+global.fakereplyt = 'Mel-bot1 WhatsApp' // Teks Untuk Fake Reply
 global.mess = {
 group : {
 welcome : `Welcome @user\n\nSelamat datang di grup @subject`, // Teks Untuk Welcome
@@ -17,21 +17,21 @@ success: 'Sukses...' // Sukses
 }
 global.server = false // true = Manyalakan server Localhost, false = menonaktifkan server Localhost
 global.prefix = '🐤' // Skip, Ga Terlalu Berguna
-global.author = '@melcanz.io' // Nama Author Sticker
+global.author = '@Aiman*͘⁴̅⁰͍⁴̵〆*' // Nama Author Sticker
 global.packname = 'WhatsApp Bot' // Nama Packname Sticker
 
 
 // LIST APIKEY
 
-global.APIs = { // API Prefix
+global.APIs = { // APIKey Here
   nrtm: 'https://nurutomo.herokuapp.com',
   zeks: 'https://api.zeks.me',
   mel: 'https://melcanz.com'
 }
 
-global.APIKeys = { // APIKey Here
+global.APIKeys = { // 4ef1b11287339c1b10699ece
   'https://api.zeks.me': 'rikkabotwa', // Free Ampikey :v
-  'https://melcanz.com': 'YOUR-APIKEY', // Buy Apikey On https://wa.me/6287755080455 Or Register On https://melcanz.com
+  'https://melcanz.com': '4ef1b11287339c1b10699ece', // Buy Apikey On https://wa.me/6287755080455 Or Register On https://melcanz.com
 }
 
 let file = require.resolve(__filename)
